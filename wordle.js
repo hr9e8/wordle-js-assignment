40287885b137388cf17329934c26bdf6ddd2636b
Hafiey Rizal <hafieyr@neurodigital.my>
2024-02-28 00:09:17 +0800

    final task completed

--- wordle.js
+++ wordle.js
@@ -10,12 +10,9 @@ const mustHaveLettersInPosition = ['s','p','i',,'e']
 // not in these positions
 const mustNotHaveLettersInPosition = [,'e',,'c','s']
 
+// function to return true if the word has any of the letters
 const containsLetter = (word, letter) => {
   return word.includes(letter);
-}
-
-const doesNotContainLetter = (word, letter) => {
-  return !word.includes(letter);
 }
 
 const hasLetterInPosition = (word, letter, position) => {
@@ -24,11 +21,14 @@ const hasLetterInPosition = (word, letter, position) => {
   return word[position] === letter;
 }
 
+//TASK: make sure that no words in 'filteredWords' have letters from the 'mustNotHaveLettersInPosition'
+
 const filteredWords = allWords.filter(word => {
   return hasLetters.every(letter => containsLetter(word, letter)) &&
-  mustNotHaveLetters.every(letter => doesNotContainLetter(word, letter)) &&
-  mustHaveLettersInPosition.every((letter, index) => hasLetterInPosition(word, letter, index))
-})
+         mustNotHaveLetters.every(letter => !containsLetter(word, letter)) &&
+         mustHaveLettersInPosition.every((letter, index) => hasLetterInPosition(word, letter, index)) &&
+         mustNotHaveLettersInPosition.every((letter, index) => letter === undefined || !hasLetterInPosition(word, letter, index));
+});
 
 console.log(filteredWords);
 console.log('possible words', filteredWords.length);
